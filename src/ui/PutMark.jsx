1edbf4e18abb0205cd177a992a309ebebbde4bba
rstulug <rstulug@gmail.com>
2024-01-15 23:16:36 +0300

    reminder

--- src/ui/PutMark.jsx
+++ src/ui/PutMark.jsx
@@ -1,23 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Marker, useMapEvent, Popup } from "react-leaflet";
 import { useReverseGeocoding } from "../hooks/useReverseGeocoding";
 
 function PutMark() {
   const [position, setPosition] = useState(null);
+  const [city, setCity] = useState(null);
 
   const map = useMapEvent("click", (e) => {
     setPosition(() => e.latlng);
     map.setView(e.latlng);
   });
 
-  console.log(position);
+  useEffect(function () {}, []);
   const { data, isLoading } =
     useReverseGeocoding({
       lat: position?.lat,
       lng: position?.lng,
     }) || {};
 
-  //console.log(data);
+  console.log(data);
 
   if (!position) return null;
   return (
