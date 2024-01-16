06c70ec8db38ddca0f34ac0ee1ed38def3218bc5
rstulug <rstulug@gmail.com>
2024-01-16 13:06:04 +0300

    some row added

--- src/ui/MemoryForm.jsx
+++ src/ui/MemoryForm.jsx
@@ -1,13 +1,36 @@
+import { useForm } from "react-hook-form";
 import { useLocation } from "../context/LocationContext";
 import Form from "./Form";
+import FormRow from "./FormRow";
 
 function MemoryForm() {
   const { location } = useLocation();
   console.log(location);
-  if (location)
+  const { register, handleSubmit, formState } = useForm();
+  const { errors } = formState;
+
+  function onSubmit() {}
+  if (location.country)
     return (
       <div>
-        <Form></Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <FormRow label="Country" error={errors?.country?.message}>
+            <input
+              type="text"
+              id="country"
+              defaultValue={location.country}
+              {...register("country")}
+            />
+          </FormRow>
+          <FormRow label="Region" error={errors?.region?.message}>
+            <input
+              type="text"
+              id="region"
+              defaultValue={location.name}
+              {...register("region")}
+            />
+          </FormRow>
+        </Form>
       </div>
     );
 }
